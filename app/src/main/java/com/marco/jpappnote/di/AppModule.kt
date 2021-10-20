e3e666bdb7d2bf615cf3551dda62a61bea5730be
MarcoMartiniano <79107727+MarcoMartiniano@users.noreply.github.com>
2021-10-20 18:27:02 -0300

    Fifth Commit
    -AppModule
    -GetNoteUseCase
    -NoteUsesCases
    -AddEditNoteEvent
    -AddEditNoteViewModel
    -NoteTextFieldState

--- app/src/main/java/com/marco/jpappnote/di/AppModule.kt
+++ app/src/main/java/com/marco/jpappnote/di/AppModule.kt
@@ -5,10 +5,7 @@ import androidx.room.Room
 import com.marco.jpappnote.feature_note.data.data_source.NoteDatabase
 import com.marco.jpappnote.feature_note.data.repository.NoteRepositoryImp
 import com.marco.jpappnote.feature_note.domain.repository.NoteRepository
-import com.marco.jpappnote.feature_note.domain.use_case.AddNoteUseCase
-import com.marco.jpappnote.feature_note.domain.use_case.DeleteNoteUseCase
-import com.marco.jpappnote.feature_note.domain.use_case.GetNotesUseCase
-import com.marco.jpappnote.feature_note.domain.use_case.NoteUsesCases
+import com.marco.jpappnote.feature_note.domain.use_case.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -41,7 +38,8 @@ object AppModule {
         return NoteUsesCases(
             getNotes = GetNotesUseCase(repository),
             deleteNote = DeleteNoteUseCase(repository),
-            addNote = AddNoteUseCase(repository)
+            addNote = AddNoteUseCase(repository),
+            getNote = GetNoteUseCase(repository)
         )
     }
 }
